a41a4e074f99a209c3e8b579e36e8cb92ad60db5
Filipe Rodrigues <rodrigues.filipe@live.com.pt>
2017-09-29 02:10:29 +0100

    % final

--- index.php
+++ index.php
@@ -55,9 +55,85 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
     <title>Simple Youtube Video Downloader</title>
 </head>
 <body>
-	<div class="container">
-		<h1>Simple Youtube Video Downloader</h1>
-	</div>
+<div class="container">
+	<br>
+    <h1>Simple Youtube Video Downloader
+	</h1>
+	<br>
+    <form action="" method="post" id="download-form">
+        <div class="row">
+            <div class="col-md-10">
+                <div class="form-group <?= $isError ? 'has-error' : '' ?>">
+                    <input id="video-url" title="Video url" type="text" name="url" placeholder="Video url" class="form-control" value="<?= htmlspecialchars($url) ?>"/>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block">Download links</button>
+                </div>
+            </div>
+        </div>
+    </form>
+    <div class="alert alert-danger" role="alert" id="error-block" style="display: <?= $isError ? 'block' : 'none' ?>">
+        <?= $isError ? $response['error'] : '' ?>
+    </div>
+
+    <?php if ($isSuccessResponse): ?>
+        <?php
+			$baseInfo = $response['data']['baseInfo'];
+			$downloadInfo = $response['data']['downloadInfo'];
+        ?>
+    <?php endif; ?>
+
+    <h3 id="video-name">
+        <?php if ($isSuccessResponse): ?>
+            <?= htmlspecialchars($baseInfo['name']) ?>
+        <?php endif; ?>
+    </h3>
+
+    <div class="row">
+        <div class="col-md-6" style="display: <?= $isSuccessResponse ? 'block' : 'none' ?>">
+            <div id="video-preview">
+                <?php if ($isSuccessResponse): ?>
+                    <img src="<?= $baseInfo['previewUrl'] ?>" alt="Video preview" class="img-responsive img-thumbnail img-rounded">
+                <?php endif; ?>
+            </div>
+            <pre id="video-description">
+                <?php if ($isSuccessResponse): ?>
+                    <?= htmlspecialchars($baseInfo['description']) ?>
+                <?php endif; ?>
+            </pre>
+        </div>
+        <div class="col-md-6">
+            <table id="download-list" class="table">
+                <thead <?= !$isSuccessResponse ? 'style="display: none"' : '' ?>>
+					<tr>
+						<th>Type</th>
+						<th>Size</th>
+						<th>Link</th>
+					</tr>
+                </thead>
+                <tbody>
+                <?php if ($isSuccessResponse): ?>
+                    <?php foreach ($downloadInfo AS $downloadInfoItem): ?>
+                        <tr>
+                            <td><?= $downloadInfoItem['fileType'] ?></td>
+                            <td><?= $downloadInfoItem['fileSizeHuman'] ?></td>
+                            <td>
+                                <?php
+                                    $downloadUrl = 'download.php?id=' . $videoId . '&itag=' . $downloadInfoItem['youtubeItag'];
+                                ?>
+                                <a href="<?= $downloadUrl ?>" target="_blank" class="btn btn-success"><span class="glyphicon glyphicon-circle-arrow-down"></span>Download</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div id="load-shadow" style="display: none; width: 100%; height: 100%; left: 0; top: 0; background: rgba(63, 0, 255, 0.31); z-index: 10; position: fixed;"></div>
 </body>
 <script src="jquery/jquery-3.2.1.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
