730c3dc75d2b6fbf8b138516ba5f7a877e37c8d6
Filipe Rodrigues <rodrigues.filipe@live.com.pt>
2017-09-29 02:00:05 +0100

    + init html

--- index.php
+++ index.php
@@ -43,3 +43,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 }
 
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+    <title>Simple Youtube Video Downloader</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Simple Youtube Video Downloader</h1>
+	</div>
+</body>
+<script src="jquery/jquery-3.2.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</html>
